9f35afabe0c372b12df207d311db19bf02d08f87
ToxiKat-dev <toxikat.dev@gmail.com>
2023-11-22 22:27:57 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tachiyone
+
+Learning flutter from making a basic manga reader using mangadex api.
---
+++ android/app/src/main/kotlin/dev/toxikat/tachione/tachiyone/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.toxikat.tachione.tachiyone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
